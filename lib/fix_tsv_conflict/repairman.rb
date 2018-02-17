1b50f4dc0f75764e16301feb071b12579f2164a2
Masato Ikeda <masato.ikeda@gmail.com>
2018-02-17 21:18:49 +0900

    Fix conflicted new lines

--- lib/fix_tsv_conflict/repairman.rb
+++ lib/fix_tsv_conflict/repairman.rb
@@ -1,7 +1,34 @@
 module FixTsvConflict
   class Repairman
     def repair(source)
-      source
+      result = []
+
+      left  = []
+      right = []
+      branch = nil
+      source.each_line do |line|
+        if branch
+          if line.start_with?(">>>>>>>")
+            result += resolve(left, right)
+            branch = nil
+          elsif line.start_with?("=======")
+            branch = right
+          else
+            branch << line
+          end
+        else
+          if line.start_with?("<<<<<<<")
+            branch = left
+          else
+            result << line
+          end
+        end
+      end
+      result.join
+    end
+
+    def resolve(left, right)
+      left + right
     end
   end
 end
--- test/repairman_test.rb
+++ test/repairman_test.rb
@@ -11,4 +11,24 @@ id\tname
     TEXT
     assert_equal source, repairman.repair(source)
   end
+
+  def test_repair_with_conflicted_new_records_for_different_ids
+    repairman = FixTsvConflict::Repairman.new
+    source = <<-TEXT
+id\tname
+1\tJess
+<<<<<<< add_danny
+2\tDanny
+=======
+3\tJoey
+>>>>>>> add_joey
+    TEXT
+    expected = <<-TEXT
+id\tname
+1\tJess
+2\tDanny
+3\tJoey
+    TEXT
+    assert_equal expected, repairman.repair(source)
+  end
 end
