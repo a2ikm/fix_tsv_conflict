62b894aaa981ec0b0ac5ac5fcfedd229dc552a9f
Masato Ikeda <masato.ikeda@gmail.com>
2018-02-18 01:13:16 +0900

    Setup guard

--- test/fix_tsv_conflict_test.rb
+++ test/fix_tsv_conflict_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
 class FixTsvConflictTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::FixTsvConflict::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
