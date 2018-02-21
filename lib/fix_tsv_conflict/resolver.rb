2d1b0a9e7f48fd616654648d3c19e9d0aee442fe
Masato Ikeda <masato.ikeda@gmail.com>
2018-02-22 04:05:31 +0900

    Roughly reimplemented

---
+++ lib/fix_tsv_conflict/resolver.rb
@@ -0,0 +1,96 @@
+require "fix_tsv_conflict/logging"
+
+module FixTSVConflict
+  class Resolver
+    include Logging
+
+    BLANK_RE = /\A[[:space:]]*\z/
+    using Module.new {
+      refine String do
+        def blank?
+          BLANK_RE === self
+        end
+      end
+    }
+
+    attr_reader :stdin, :stderr
+
+    def initialize(stdin: $stdin, stderr: $stderr)
+      @stdin  = stdin
+      @stderr = stderr
+    end
+
+    def resolve(conflict)
+      unless conflict.valid?
+        return conflict.to_a
+      end
+
+      result = try(conflict)
+      if result
+        return result
+      end
+
+      warn "Automatic repair failed."
+      select(conflict)
+    end
+
+    def try(conflict)
+      result = []
+      left  = index_by_id(conflict.left.reject(&:blank?))
+      right = index_by_id(conflict.right.reject(&:blank?))
+      (left.keys + right.keys).uniq.sort.each do |id|
+        l = left[id]
+        r = right[id]
+        if l && r
+          if l.rstrip == r.rstrip
+            result << l # FIXME select better one from l and r
+          else
+            return false
+          end
+        else
+          result << (l || r)
+        end
+      end
+      result
+    end
+
+    def select(conflict)
+      text = <<-TEXT
+Which do you want keep?
+
+  1) #{conflict.lbranch}
+  2) #{conflict.rbranch}
+  k) keep as is
+
+      TEXT
+
+      info text
+
+      loop do
+        info "Please enter 1, 2, or k: ", no_newline: true
+        case selected = stdin.gets.strip
+        when "1"
+          return conflict.left
+        when "2"
+          return conflict.right
+        when "k"
+          return conflict.to_a
+        else
+          text = <<-TEXT
+Invalid input: #{selected}
+          TEXT
+          stderr.print text.chomp
+        end
+      end
+    end
+
+    def index_by_id(lines)
+      result = {}
+      lines.each do |line|
+        id = line.split(TAB, 2).first
+        result[id] = line
+      end
+      result
+    end
+  end
+end
